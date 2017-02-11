47b1b6b3a76336ad182baa264318a5b227483202
gnzandrs <gavr@outlook.cl>
2017-02-10 23:30:48 -0300

    working with prototype for efficiency

--- js/cat.js
+++ js/cat.js
@@ -1,25 +1,12 @@
 // construct
-var Cat = function (name, age) {
-
+function Cat (name, age ) {
   this.name = name;
   this.age = age;
+};
 
-  if (typeof this.name != 'string')
-  {
-    message.innerHTML = 'The propety name have to be a string';
-    exit();
-  }
-
-  if (typeof this.age != 'number')
-  {
-    message.innerHTML = 'The propety age have to be a number';
-    exit();
-  }
-
-  // methods
-  this.eat = function()
-  {
+Cat.prototype = {
+  eat: function() {
     message.innerHTML = 'My dear cat ' + this.name + ' ate a cookie!';
     document.getElementById('imageAction').src = 'img/eating.gif';
   }
-}
+};
