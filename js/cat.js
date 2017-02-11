997e79beab5cb9d5fa36906a3fbd71be1ae25637
gnzandrs <gavr@outlook.cl>
2017-02-11 18:47:37 -0300

    new methods

--- js/cat.js
+++ js/cat.js
@@ -2,11 +2,31 @@
 function Cat (name, age ) {
   this.name = name;
   this.age = age;
+
+  if (typeof this.name != 'string') {
+		message.innerHTML = 'The propety name have to be a string';
+		exit();
+	}
+
+  if (typeof this.age != 'number') {
+    message.innerHTML = 'The propety age have to be a number';
+    exit();
+  }
 };
 
 Cat.prototype = {
-  eat: function() {
+  dance: function () {
+    message.innerHTML = 'My dear cat ' + this.name + ' is dancing!';
+    document.getElementById('imageAction').src = 'img/eating.gif';
+  },
+
+  eat: function () {
     message.innerHTML = 'My dear cat ' + this.name + ' ate a cookie!';
     document.getElementById('imageAction').src = 'img/eating.gif';
+  },
+
+  sleep: function () {
+    message.innerHTML = 'My dear cat ' + this.name + ' is taking a nap!';
+    document.getElementById('imageAction').src = 'img/sleeping.gif';
   }
 };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-function despertar ()
-{
+function init () {
   var message = document.getElementById('message');
-
   // create and call the object
   var Minino = new Cat('Minino', 13);
   message.innerHTML = Minino.name + ' was a very cute cat and he live wonderful until the age of ' + Minino.age;
   Minino.eat();
-
+  var initButton = document.getElementById('initButton');
+  initButton.style.display = 'none';
 }
